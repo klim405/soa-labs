33d876aa3dbfbf326fcd1529ad8de1b47f2a462d
Diosa34 <superalex.osa@yandex.ru>
2024-10-17 14:02:02 +0300

    Initial commit

---
+++ src/main/java/com/musicbands/bandgrammyservice/BandGrammyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.musicbands.bandgrammyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandGrammyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandGrammyServiceApplication.class, args);
+    }
+
+}
